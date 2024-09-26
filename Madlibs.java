2ad7a7253d9f78651a6160766509a30c8f8661b9
drewcmarkham <drew.markham@icloud.com>
2024-09-26 08:59:40 -0700

    main class and scanner class for name input

---
+++ Madlibs.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Madlibs {
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter name: ");
+
+        String name = sc.nextLine();
+        System.out.println("username: " + name);
+        
+    }
+
+}
