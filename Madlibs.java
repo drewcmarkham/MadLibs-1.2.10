3cbfe277b81e176f550875ff76015c07cf174cc5
drewcmarkham <drew.markham@icloud.com>
2024-09-26 09:34:27 -0700

    find opening and closing numbers of speech. Calculate length of speech

--- Madlibs.java
+++ Madlibs.java
@@ -1,16 +1,32 @@
 
-import java.util.Scanner;
 
 public class Madlibs {
     
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter name: ");
 
-        String name = sc.nextLine();
-        System.out.println("username: " + name);
-        
+        String madlib1 = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+        // String madlib2 = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        // String madlib3 = "There was a time when <plural_noun> would roam the <noun> and <verb>";
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter name: ");
+
+        // String name = sc.nextLine();
+        // System.out.println("username: " + name);
+
+        int speech1open = madlib1.indexOf("<");
+        int speech1close = madlib1.indexOf(">");
+        int speech2open = madlib1.indexOf("<", madlib1.indexOf("<") + 1);
+        int speech2close = madlib1.indexOf(">", madlib1.indexOf(">") + 1);
+
+        int speech1length = speech1close - speech1open;
+        int speech2length = speech2close - speech2open;
+
+        System.out.println(speech1length);
+        System.out.println(speech2length);
+
+
+
     }
 
 }
